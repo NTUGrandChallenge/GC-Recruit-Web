1ce37691074b6c17c4c31fd60be90af739d6a4d6
XXLee <leechinghon@gmail.com.tw>
2016-09-30 19:56:53 +0800

    update navbar

--- recruitsite/settings.py
+++ recruitsite/settings.py
@@ -96,7 +96,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'test1',
-        
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
