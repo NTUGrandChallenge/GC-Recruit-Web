a72d38c1ad6169b68ce24d774e4bcd3eba087f88
anonymous28456 <anonymous28456@gmail.com>
2016-09-30 17:03:03 +0800

    basic forum added to :8000/forum/

--- recruitsite/settings.py
+++ recruitsite/settings.py
@@ -47,6 +47,18 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.facebook',
     #'allauth.socialaccount.providers.google',
     #'allauth.socialaccount.providers.twitter',
+
+    #lbforum
+    'el_pagination',
+    'easy_thumbnails',
+    'constance',
+    'constance.backends.database',
+    'djangobower',
+
+    'lbforum',
+    'lbattachment',
+    'lbutils',
+
 ]
 
 SITE_ID = 1
@@ -94,8 +106,14 @@ WSGI_APPLICATION = 'recruitsite.wsgi.application'
 
 DATABASES = {
     'default': {
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': 'test1',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test1',
+                   'NAME': 'gcdb',
+                    'USER': 'gc',
+                    'PASSWORD': 'gc',
+                      'HOST': 'localhost',
+                       'PORT ':'',
         
     }
 }
@@ -180,3 +198,62 @@ LOGIN_REDIRECT_URL = "/index/"
 #AUTH_PROFILE_MODULE = 'profiles.Student'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
+
+
+#lbforum
+HOST_URL = ''
+MEDIA_URL_ = '/media/'
+MEDIA_URL = HOST_URL + MEDIA_URL_
+
+SIGNUP_URL = '/forum/accounts/signup/'
+LOGIN_URL = '/forum/accounts/login/'
+LOGOUT_URL = '/forum/accounts/logout/'
+LOGIN_REDIRECT_URL = '/forum/'
+CHANGE_PASSWORD_URL = '/forum/accounts/password/change/'
+
+CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
+
+CONSTANCE_CONFIG = {
+      'forbidden_words': ('', 'Forbidden words', str),
+}
+
+from django.conf.global_settings import STATICFILES_FINDERS
+STATICFILES_FINDERS += (('djangobower.finders.BowerFinder'),)
+
+
+BOWER_INSTALLED_APPS = (
+    'jquery#1.12',
+        'markitup#1.1.14',
+            'mediaelement#2.22.0',
+                'blueimp-file-upload#9.12.5',
+                )
+
+BBCODE_AUTO_URLS = True
+#add allow tags
+HTML_SAFE_TAGS = ['embed']
+HTML_SAFE_ATTRS = ['allowscriptaccess', 'allowfullscreen', 'wmode']
+#add forbid tags
+HTML_UNSAFE_TAGS = []
+HTML_UNSAFE_ATTRS = []
+
+"""
+#default html safe settings
+acceptable_elements = ['a', 'abbr', 'acronym', 'address', 'area', 'b', 'big',
+    'blockquote', 'br', 'button', 'caption', 'center', 'cite', 'code', 'col',
+        'colgroup', 'dd', 'del', 'dfn', 'dir', 'div', 'dl', 'dt', 'em',
+            'font', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img',
+                'ins', 'kbd', 'label', 'legend', 'li', 'map', 'menu', 'ol',
+                    'p', 'pre', 'q', 's', 'samp', 'small', 'span', 'strike',
+                        'strong', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th',
+                            'thead', 'tr', 'tt', 'u', 'ul', 'var']
+                            acceptable_attributes = ['abbr', 'accept', 'accept-charset', 'accesskey',
+                                'action', 'align', 'alt', 'axis', 'border', 'cellpadding', 'cellspacing',
+                                    'char', 'charoff', 'charset', 'checked', 'cite', 'clear', 'cols',
+                                        'colspan', 'color', 'compact', 'coords', 'datetime', 'dir',
+                                            'enctype', 'for', 'headers', 'height', 'href', 'hreflang', 'hspace',
+                                                'id', 'ismap', 'label', 'lang', 'longdesc', 'maxlength', 'method',
+                                                    'multiple', 'name', 'nohref', 'noshade', 'nowrap', 'prompt',
+                                                        'rel', 'rev', 'rows', 'rowspan', 'rules', 'scope', 'shape', 'size',
+                                                            'span', 'src', 'start', 'summary', 'tabindex', 'target', 'title', 'type',
+                                                                'usemap', 'valign', 'value', 'vspace', 'width', 'style']
+                                                                """
