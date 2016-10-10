41372410653b4a8d35aba164789f8c803f6edae0
XXLee <leechinghon@gmail.com.tw>
2016-10-10 17:19:12 +0800

    update edit & create student

--- static/js/create_student.js
+++ static/js/create_student.js
@@ -20,27 +20,16 @@ $('#create_student .one-talent .category select').change(function(){
 	$(this).parents('.one-talent').find('.group select option').css('display', 'none');
 	$(this).parents('.one-talent').find('.group select option.son-of-c-' + $(this).find(':selected').attr('data-id')).css('display', 'inherit').first().prop('selected', 'true');
 	$(this).parents('.one-talent').find('.group select').trigger('change');
-	// $(this).parents('.one-talent').find('.group select').css('display', 'none').find('select').attr('name', 'groupX');
-	// $(this).parents('.one-talent').find('.bootstrap-select.son-of-c-' + ($(this).find(':selected').attr('data-id'))).css('display', 'inherit')
-	// .find('select').attr('name', 'group').find('option:first-child').attr('selected', true);
-	console.log($(this).find(':selected').text());
 });
 $('#create_student .one-talent .group select').change(function(){
 	$(this).parents('.one-talent').find('.talent select option').css('display', 'none');
 	$(this).parents('.one-talent').find('.talent select option.son-of-g-' + $(this).find(':selected').attr('data-id')).css('display', 'inherit').first().prop('selected', 'true');
-// 	console.log("GG");
-// 	$(this).parents('.one-talent').find('.talent .bootstrap-select').css('display', 'none').find('select').attr('name', 'talentX');
-// 	$(this).parents('.one-talent').find('.bootstrap-select.son-of-g-' + ($(this).find(':selected').attr('data-id'))).css('display', 'inherit')
-// 	.find('select').attr('name', 'talent[]').find('option:nth-child(1)').attr('selected', true);
-});
-$('#create_student .one-talent select[name="talent[]"]').change(function(){
-	$(this).parents('.one-talent').find('.add-talent').css('display', 'initial');
 });
 
 // add talent
 $('#create_student .one-talent .add-talent').on('click', function(){
-	$(this).parents('.one-talent').next('.one-talent').css('display', 'block');
-	$(this).css('display', 'none');
+	$(this).parents('.one-talent').next('.one-talent').removeClass('hide')
+	$(this).addClass('hide');
 });
 
 
