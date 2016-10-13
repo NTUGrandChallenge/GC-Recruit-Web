ce065c5dc318d6fc3156bc1452e1078f4b612abe
XXLee <leechinghon@gmail.com.tw>
2016-10-14 03:30:40 +0800

    update my_profile

---
+++ static/js/profile.js
@@ -0,0 +1,13 @@
+var exp_height = $('#profile .exp .content p').height() + 25;
+$(window).load(function(){
+	if(exp_height > 100){
+	    $('#profile .exp').height(exp_height);
+	    $('#profile .exp .title').height(exp_height);
+	}
+});
+$(window).on('resize', function(){
+    if(exp_height > 100){
+	    $('#profile .exp').height(exp_height);
+	    $('#profile .exp .title').height(exp_height);
+	}
+});
