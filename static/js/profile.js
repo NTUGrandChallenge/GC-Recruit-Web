91847fb9d270f6d80ec16d476ddd2e53bc92ed14
XXLee <leechinghon@gmail.com.tw>
2016-10-14 04:41:15 +0800

    update my_profile

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,5 +1,7 @@
 var exp_height = $('#profile .exp .content p').height() + 25;
 $(window).load(function(){
+	$('#profile #sidebar').height($('#challenger_profile').height());
+
 	if(exp_height > 100){
 	    $('#profile .exp').height(exp_height);
 	    $('#profile .exp .title').height(exp_height);
